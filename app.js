4f10c52a422d0de640689d43e8d8979924f679d4
Sergey Sherby <sergey.sherby@sportsfusion.co.uk>
2020-01-11 15:07:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+app.use(function(req, res, next) {
+  res.sendStatus(404);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
+
+module.exports = app;
